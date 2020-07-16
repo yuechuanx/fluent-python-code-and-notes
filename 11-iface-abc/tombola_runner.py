764518ce8a8dfcc236387374ecc637b9b9b6f997
Yuechuan Xiao <xiaoyuechuanz@163.com>
2020-07-16 16:51:20 +0800

    docs: update docs

--- 11-iface-abc/tombola_runner.py
+++ 11-iface-abc/tombola_runner.py
@@ -4,7 +4,10 @@ import doctest
 from tombola import Tombola
 
 # modules to test
-import bingo, lotto, tombolist, drum  # <1>
+import bingo
+import lotto
+import tombolist
+import drum  # <1>
 
 TEST_FILE = 'tombola_tests.rst'
 TEST_MSG = '{0:16} {1.attempted:2} tests, {1.failed:2} failed - {2}'
@@ -13,19 +16,21 @@ TEST_MSG = '{0:16} {1.attempted:2} tests, {1.failed:2} failed - {2}'
 def main(argv):
     verbose = '-v' in argv
     real_subclasses = Tombola.__subclasses__()  # <2>
-    virtual_subclasses = list(Tombola._abc_registry)  # <3>
+    # virtual_subclasses = list(Tombola._abc_registry)  # <3>
+    # virtual_subclasses = list(Tombola._abc_impl)  # <3>
 
-    for cls in real_subclasses + virtual_subclasses:  # <4>
+    # for cls in real_subclasses + virtual_subclasses:  # <4>
+    for cls in real_subclasses:  # <4>
         test(cls, verbose)
 
 
 def test(cls, verbose=False):
 
     res = doctest.testfile(
-            TEST_FILE,
-            globs={'ConcreteTombola': cls},  # <5>
-            verbose=verbose,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE)
+        TEST_FILE,
+        globs={'ConcreteTombola': cls},  # <5>
+        verbose=verbose,
+        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE)
     tag = 'FAIL' if res.failed else 'OK'
     print(TEST_MSG.format(cls.__name__, res, tag))  # <6>
 
